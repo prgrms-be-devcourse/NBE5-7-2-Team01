e4657a5ca21dc0188be9bfe8459db06a65b7c80c
hs986 <94986211+hs986@users.noreply.github.com>
2025-05-09 17:32:12 +0900

    fix : builder 어노테이션 추가

--- src/main/java/com/fifo/ticketing/domain/performance/entity/Performance.java
+++ src/main/java/com/fifo/ticketing/domain/performance/entity/Performance.java
@@ -3,6 +3,7 @@ package com.fifo.ticketing.domain.performance.entity;
 import com.fifo.ticketing.global.entity.BaseDateEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Table(name = "performance")
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Performance extends BaseDateEntity {
 
     @Id
@@ -44,4 +46,6 @@ public class Performance extends BaseDateEntity {
 
     @Column(nullable = false)
     private LocalDateTime reservationStartTime;
+
+
 }
