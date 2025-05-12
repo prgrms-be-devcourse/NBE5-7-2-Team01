0f7681124c228a8fc824cb203e300b96dab71262
hs986 <94986211+hs986@users.noreply.github.com>
2025-05-12 15:09:36 +0900

    feat : performance 에 좋아요 누른 유저 찾기

--- src/main/java/com/fifo/ticketing/domain/like/repository/LikeRepository.java
+++ src/main/java/com/fifo/ticketing/domain/like/repository/LikeRepository.java
@@ -3,10 +3,17 @@ package com.fifo.ticketing.domain.like.repository;
 import com.fifo.ticketing.domain.like.entity.Like;
 import com.fifo.ticketing.domain.performance.entity.Performance;
 import com.fifo.ticketing.domain.user.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
     Like findByUserAndPerformance(User user, Performance performance);
+
+
+    @Query("SELECT l.user FROM Like l WHERE l.performance.id = :performanceId AND l.isLiked = true")
+    List<User> findUsersByPerformanceId(@Param("performanceId") Long performanceId);
 }
