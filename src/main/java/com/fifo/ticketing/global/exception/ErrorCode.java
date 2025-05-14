8df9bd8d16db24a76ad111a505cfe4f244b7586a
chaechae <juold0568@naver.com>
2025-05-14 17:57:39 +0900

    feat: 스케줄러 관련 에러코드 추가

--- src/main/java/com/fifo/ticketing/global/exception/ErrorCode.java
+++ src/main/java/com/fifo/ticketing/global/exception/ErrorCode.java
@@ -26,7 +26,8 @@ public enum ErrorCode {
     EMAIL_ALREADY_EXISTS("EMAIL-001", "이미 사용하는 이메일입니다.", ALREADY_EXISTS),
     NOT_FOUND_AUTH("AUTH-002", "잘못된 인증번호 입니다.", NOT_FOUND),
     NOT_FOUND_PROVIDER("PROVIDER-001", "지원하지 않는 플랫폼 서비스입니다.", NOT_FOUND),
-    NOT_FOUND_BOOK("BOOK-001", "예약 정보가 존재하지 않습니다.", NOT_FOUND);
+    NOT_FOUND_BOOK("BOOK-001", "예약 정보가 존재하지 않습니다.", NOT_FOUND),
+    NOT_FOUND_SCHEDULE("SCHEDULE-001", "존재하지 않는 스케줄 업무입니다.", NOT_FOUND);
 
 
     private final String code;
