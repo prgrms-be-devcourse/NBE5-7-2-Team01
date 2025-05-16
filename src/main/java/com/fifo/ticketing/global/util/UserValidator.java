299c9d71c43caa297e96aff805fee3ec41c1a163
chaechae <juold0568@naver.com>
2025-05-16 09:09:58 +0900

    feat: UserValidator SessionUser 반환

--- src/main/java/com/fifo/ticketing/global/util/UserValidator.java
+++ src/main/java/com/fifo/ticketing/global/util/UserValidator.java
@@ -10,12 +10,12 @@ public class UserValidator {
     private UserValidator() {
     }
 
-    public static Long validateSessionUser(HttpSession session) {
+    public static SessionUser validateSessionUser(HttpSession session) {
         SessionUser user = (SessionUser) session.getAttribute("loginUser");
         if (user == null) {
             throw new ErrorException("user/sign_in", ErrorCode.UNAUTHORIZED_REQUEST);
         }
 
-        return user.id();
+        return user;
     }
 }
