114aa5333e7c6e033a6d69d82d3ef98d4e65449e
hs986 <94986211+hs986@users.noreply.github.com>
2025-05-19 11:07:56 +0900

    feat : 메일 mapper 추가

---
+++ src/main/java/com/fifo/ticketing/domain/like/mapper/LikeMailMapper.java
@@ -0,0 +1,27 @@
+package com.fifo.ticketing.domain.like.mapper;
+
+import com.fifo.ticketing.domain.like.dto.NoPayedMailDto;
+import com.fifo.ticketing.domain.like.dto.ReservationStartMailDto;
+import com.fifo.ticketing.domain.performance.entity.Performance;
+import com.fifo.ticketing.domain.user.entity.User;
+
+public class LikeMailMapper {
+    public static ReservationStartMailDto toReservationStartMailDto(User user, Performance performance) {
+        return ReservationStartMailDto.builder()
+            .email(user.getEmail())
+            .username(user.getUsername())
+            .performanceTitle(performance.getTitle())
+            .reservationStartTime(performance.getReservationStartTime()) // 필드명에 맞게 조정
+            .build();
+    }
+
+    public static NoPayedMailDto toNoPayedMailDto(User user, Performance performance, int availableSeats) {
+        return NoPayedMailDto.builder()
+            .email(user.getEmail())
+            .username(user.getUsername())
+            .performanceTitle(performance.getTitle())
+            .availableSeats(availableSeats)
+            .build();
+    }
+
+}
